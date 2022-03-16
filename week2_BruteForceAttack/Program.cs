da4dfa2c3c1897371560a86ea0bc148540522fb1
momo990305@gmail.com <momo990305@gmail.com>
2022-03-17 01:18:09 +0900

    sync

--- week2_BruteForceAttack/Program.cs
+++ week2_BruteForceAttack/Program.cs
@@ -4,6 +4,11 @@ namespace week2_BruteForceAttack
 {
     class MainClass
     {
+        public void Generate_Password()
+        {
+
+        }
+
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
