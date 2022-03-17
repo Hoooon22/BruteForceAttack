48410f6941eebe8c7cd1fd6a06acc12bc46c8e66
momo990305@gmail.com <momo990305@gmail.com>
2022-03-18 00:48:27 +0900

    writing Generate_password

--- week2_BruteForceAttack/Program.cs
+++ week2_BruteForceAttack/Program.cs
@@ -4,14 +4,36 @@ namespace week2_BruteForceAttack
 {
     class MainClass
     {
-        public void Generate_Password()
+        public static string Generate_Password(int type)
         {
+            string password = "";
 
+            switch (type)
+            {
+                case 0:
+
+
+                default:
+                    break;
+            }
+
+            return password;
         }
 
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string password; // 입력할 패스워드
+            int pw_type; // 입력받은 패스워드의 유형
+            int pw_length; // 입력받은 패스워드의 길이
+            string key; // 생성한 모든 경우의 문
+
+            // 1. 무작위 패스워드 입력
+            System.Console.WriteLine("0. 숫자, 1. 알파벳, 2. 숫자+알파벳, 3. 숫자+알파벳+특수문자");
+            System.Console.WriteLine("패스워드 유형을 선택하세요.");
+            pw_type = int.Parse(Console.ReadLine());
+            password = Generate_Password(pw_type);
+
+            // 2. 
         }
     }
 }
